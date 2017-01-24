275600b5ee397f4c93cbdbfce777f86e6620134b
Daniel Sont <dan.sont@gmail.com>
2017-01-24 14:09:20 +0900

    better output

--- main.go
+++ main.go
@@ -3,9 +3,7 @@ package main
 import (
 	"context"
 	"fmt"
-	"io"
 	"log"
-	"os"
 	"os/exec"
 
 	"github.com/docker/docker/api/types"
@@ -35,18 +33,19 @@ func DockerImages() error {
 
 	fmt.Println("List of Docker Images")
 	for _, image := range images {
-		fmt.Println("\t", image)
-		r, err := cli.ImageSave(context.Background(), []string{image.ID})
-		if err != nil {
-			return errors.Wrapf(err, "Could not save image %s to tar", image.ID)
-		}
-		outFile, err := os.Create(image.ID + ".tar")
-		// handle err
-		defer outFile.Close()
-		_, err = io.Copy(outFile, r)
-		if err != nil {
-			return errors.Wrapf(err, "io.Copy error %s", image.ID)
-		}
+		fmt.Println("\t", image.ID, image.Labels, image.RepoTags)
+		fmt.Printf("\t\t size: %d\n", image.Size)
+		// r, err := cli.(context.Background(), []string{image.ID})
+		// if err != nil {
+		// 	return errors.Wrapf(err, "Could not save image %s to tar", image.ID)
+		// }
+		// outFile, err := os.Create(image.ID + ".tar")
+		// // handle err
+		// defer outFile.Close()
+		// _, err = io.Copy(outFile, r)
+		// if err != nil {
+		// 	return errors.Wrapf(err, "io.Copy error %s", image.ID)
+		// }
 	}
 
 	fmt.Println(exec.Command("ls -l").Output())
