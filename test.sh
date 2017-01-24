1e9794e34919c4c3db9383618d800d926360fa75
Daniel Sont <dan.sont@gmail.com>
2017-01-25 00:10:27 +0900

    trying with docker ps

--- test.sh
+++ test.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
+
 docker build -t codeship-dind /test-root
 docker run -d --name dind-test-postgres postgres
+docker ps
 docker run --rm -i --link dind-test-postgres:dind-test-postgres codeship-dind
+docker ps
 docker rm -f dind-test-postgres
+docker ps
